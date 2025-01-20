9fd6033435f107ff0e7b4b34cb55a34996b8a6fd
JeanProvost <jprov310@gmail.com>
2025-01-19 20:20:53 -0700

    Updating `DbSets` in `ApplicationDbContext`

--- ChuckIt.Infrastructure/Data/ApplicationDbContext.cs
+++ ChuckIt.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ChuckItApiV2.Core.Entities;
+using ChuckItApiV2.Core.Entities.Category;
+using ChuckItApiV2.Core.Entities.Listing;
+using ChuckItApiV2.Core.Entities.Message;
+using ChuckItApiV2.Core.Entities.User;
+using Microsoft.EntityFrameworkCore;
 //using ChuckItApiV2.Entities;
 using System;
 using System.Collections.Generic;
@@ -12,6 +17,9 @@ namespace ChuckItApiV2.Infrastructure.Data
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
-        //public DbSet<Users> Users { get; set; }
+        public DbSet<Users> Users { get; set; }
+        public DbSet<Listings> Listings { get; set; }
+        public DbSet<Categories> Categories { get; set; }
+        public DbSet<Messages> Messages { get; set; }
     }
 }
