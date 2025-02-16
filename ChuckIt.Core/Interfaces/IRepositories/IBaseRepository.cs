5a80e3bdb02c3ec86cf8c8bb38f7b6e20cbd3bbf
JeanProvost <jprov310@gmail.com>
2025-02-16 12:40:19 -0700

    Creating base repository

---
+++ ChuckIt.Core/Interfaces/IRepositories/IBaseRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChuckIt.Core.Interfaces.IRepositories
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        Task<T> Create(T model);
+        Task<T> Update(T model);
+        Task Delete(T model);
+    }
+}
---
+++ ChuckIt.Infrastructure/Repositories/BaseRepository.cs
@@ -0,0 +1,39 @@
+﻿using ChuckIt.Core.Interfaces.IRepositories;
+using ChuckItApiV2.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChuckIt.Infrastructure.Repositories
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+        protected readonly ApplicationDbContext _context;
+        protected DbSet<T> Dbset => _context.Set<T>();
+
+        public BaseRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<T> Create(T model)
+        {
+            await _context.Set<T>().AddAsync(model);
+            await _context.SaveChangesAsync();
+            return model;
+        }
+
+        public async Task<T> Update(T model)
+        {
+            _context.Entry(model).State = EntityState.Modified;
+
+            await _context.SaveChangesAsync();
+
+            return model;
+        }
+
+        public async Task Delete(T model)
+        {
+            _context.Set<T>().Remove(model);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
