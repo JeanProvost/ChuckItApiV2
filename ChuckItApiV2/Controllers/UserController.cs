39f04e7075d7083ee6fc74010cc0f021332026b2
JeanProvost <jprov310@gmail.com>
2025-02-17 15:49:07 -0700

    Email verification and code resend

--- ChuckItApiV2/Controllers/UserController.cs
+++ ChuckItApiV2/Controllers/UserController.cs
@@ -2,6 +2,7 @@
 using ChuckIt.Core.Interfaces.IServices;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
 
 namespace ChuckItApiV2.Controllers
 {
@@ -28,6 +29,55 @@ namespace ChuckItApiV2.Controllers
             var result = await _authService.RegisterUserAsync(request);
 
             return Ok(result);
+        }
+
+        [HttpGet("verify-email")]
+        [AllowAnonymous]
+        public async Task<IActionResult> VerifyEmail([FromBody] VerifyEmailDto request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var result = await _authService.VerifyEmailAsync(request);
+
+            return Ok("Email successfully verified");
+        }
+
+        [HttpPost("resend-confirmation-code")]
+        [AllowAnonymous]
+        public async Task<IActionResult> ResendConfirmationCode([FromBody] ResendConfirmationDto request)
+        {
+            var result = await _authService.ResendConfirmationCodeAsync(request.Email);
+
+            return Ok("Verification code resent successfully");
+        }
+
+        [HttpPost("login")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login([FromBody] LoginDto request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var response = await _authService.LoginAsync(request);
+
+                return Ok(new
+                {
+                    AccessToken = response.AccessToken,
+                    RefreshToken = response.RefreshToken,
+                    ExpiresIn = response.ExpiresIn
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
