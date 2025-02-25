a5ef242423ca8df45e95e634121372677a4b6eed
JeanProvost <jprov310@gmail.com>
2025-02-24 20:52:09 -0700

    Implementing listing service and Dtos

---
+++ ChuckIt.Core/Services/ListingService.cs
@@ -0,0 +1,14 @@
+﻿using ChuckIt.Core.Interfaces.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChuckIt.Core.Services
+{
+    public class ListingService : IListingService
+    {
+
+    }
+}
---
+++ ChuckIt.Infrastructure/Repositories/ListingRepository.cs
@@ -0,0 +1,14 @@
+﻿using ChuckIt.Core.Interfaces.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChuckIt.Infrastructure.Repositories
+{
+    public class ListingRepository : IListingRepository
+    {
+
+    }
+}
