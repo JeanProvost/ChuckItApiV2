e15ca36b386c961d594c8252d7306fbcb800c245
JeanProvost <jprov310@gmail.com>
2025-03-01 12:30:00 -0700

    Updating `CreateListingDto`

--- ChuckIt.Core/Entities/Listings/Dtos/CreateListingDto.cs
+++ ChuckIt.Core/Entities/Listings/Dtos/CreateListingDto.cs
@@ -8,7 +8,6 @@ namespace ChuckIt.Core.Entities.Listings.Dtos
 {
     public class CreateListingDto
     {
-        public Guid Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public int CategoryId { get; set; }
