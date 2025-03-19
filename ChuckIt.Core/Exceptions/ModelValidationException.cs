44ad9d1fdb298faa20a07b1b79fad1a158b1092a
JeanProvost <jprov310@gmail.com>
2025-03-18 20:05:42 -0600

    Adding validation configuration

---
+++ ChuckIt.Core/Exceptions/ModelValidationException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChuckIt.Core.Exceptions
+{
+    public class ModelValidationException : Exception
+    {
+        public List<string> Errors { get; set; } = [];
+        public ModelValidationException() { }
+        public ModelValidationException(string message) : base(message) { }
+        public ModelValidationException(List<string> errors) : base("Validation errors have occurred")
+        {
+            Errors = errors;
+        }
+    }
+}
---
+++ ChuckItApiV2/Validators/ValidatorExtensions.cs
@@ -0,0 +1,33 @@
+﻿using ChuckIt.Core.Exceptions;
+using FluentValidation;
+using FluentValidation.Results;
+using System.ComponentModel.DataAnnotations;
+
+namespace ChuckItApiV2.Validators
+{
+    public class ValidatorExtensions
+    {
+        public static void ThrowModelValidation(FluentValidation.Results.ValidationResult validationResult)
+        {
+            List<string> validationErrors = new List<string>();
+
+            foreach (var error in validationResult.Errors)
+            {
+                validationErrors.Add(error.ErrorMessage);
+            }
+
+            throw new ModelValidationException(validationErrors);
+        }
+
+        public static void ValidateAndThrowIfInvalid<TValidator, TModel>(TValidator validator, TModel model)
+            where TValidator : IValidator<TModel>
+        {
+            var validationResult = validator.Validate(model); 
+
+            if (!validationResult.IsValid)
+            {
+                ThrowModelValidation(validationResult);
+            }
+        }
+    }
+}
