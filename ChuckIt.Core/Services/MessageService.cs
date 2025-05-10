5d6643ffbf869c46fea6659c1c9fa3d1ffd6ec00
JeanProvost <jprov310@gmail.com>
2025-05-10 13:26:49 -0600

    Initial implementation of `Message` service and repository

---
+++ ChuckIt.Core/Services/MessageService.cs
@@ -0,0 +1,20 @@
+﻿using ChuckIt.Core.Interfaces.IRepositories;
+using ChuckIt.Core.Interfaces.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChuckIt.Core.Services
+{
+    public class MessageService : IMessageService
+    {
+        private readonly IMessageRepository _messageRepository;
+
+        public MessageRepository(IMessageRepository messageRepository)
+        {
+            _messageRepository = messageRepository;
+        }
+    }
+}
---
+++ ChuckIt.Infrastructure/Repositories/MessageRepository.cs
@@ -0,0 +1,18 @@
+﻿using ChuckItApiV2.Core.Entities.Messages;
+using ChuckItApiV2.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChuckIt.Infrastructure.Repositories
+{
+    public class MessageRepository : BaseRepository<Message>
+    {
+        public MessageRepository(ApplicationDbContext context) : base(context)
+        {
+
+        }
+    }
+}
